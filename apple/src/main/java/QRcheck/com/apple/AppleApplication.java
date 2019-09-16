077658bb6ab7dd3fc19b20a811b09e4f76161321
sroy96 <sauravroy.eee@gmail.com>
2019-09-17 01:24:44 +0530

    final

---
+++ apple/src/main/java/QRcheck/com/apple/AppleApplication.java
@@ -0,0 +1,16 @@
+package QRcheck.com.apple;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@Slf4j
+@SpringBootApplication
+public class AppleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppleApplication.class, args);
+	}
+
+}
